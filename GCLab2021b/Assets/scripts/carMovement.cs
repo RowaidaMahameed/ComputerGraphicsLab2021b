3400ecd06998c6735ad3d214ead5ae6dbf49a0bc
RowaidaMahameed <79975439+RowaidaMahameed@users.noreply.github.com>
2021-06-15 17:12:15 +0300

    roadsduplicate

--- GCLab2021b/Assets/scripts/carMovement.cs
+++ GCLab2021b/Assets/scripts/carMovement.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class carMovement : MonoBehaviour
 {
+    public GameObject[] Roads = new GameObject[3];
+    public int roadIndex = 0;
     public GameObject car;
+    public GameObject mycamera;
+    public float distance = 0;
+    public float speed = 15;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +19,15 @@ public class carMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        car.gameObject.transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
+        mycamera.gameObject.transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
+        distance += speed * Time.deltaTime;
+        if(distance >= 30)
+        {
+            distance = 0;
+            Roads[roadIndex].gameObject.transform.position += new Vector3(0, 0, 90);
+            roadIndex = (roadIndex + 1) % 3;
 
-        car.gameObject.transform.position += new Vector3(0, 0, 0.1f);
+        }
     }
 }
