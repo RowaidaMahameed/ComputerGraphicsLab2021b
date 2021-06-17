6ec8a75bc4ba85e0eb033ee517757723fdf481d5
RowaidaMahameed <79975439+RowaidaMahameed@users.noreply.github.com>
2021-06-17 21:58:57 +0300

    hereweGo

--- GCLab2021b/Assets/scripts/carMovement.cs
+++ GCLab2021b/Assets/scripts/carMovement.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 using System;
 
 
-public class MyStuff
+public class MyStuff : MonoBehaviour
 {
-    public GameObject type;
+    public int type;
     public int price;
     public int weight;
-    public MyStuff(int newprice, int newweight)
+    public MyStuff(int newtype, int newprice, int newweight)
     {
+        type = newtype;
         price = newprice;
         weight = newweight;
     }
@@ -19,6 +20,11 @@ public class MyStuff
 
 public class carMovement : MonoBehaviour
 {
+    const int stuffsNumber = 3;
+    public GameObject[] allStuff = new GameObject[stuffsNumber];
+    public MyStuff stuff1 = new MyStuff(0,0,0);
+    public MyStuff stuff2 = new MyStuff(1,0, 0);
+    public MyStuff stuff3 = new MyStuff(2,0, 0);
     const int roadsNumber = 10;
     public GameObject[] Roads = new GameObject[roadsNumber];
     public int roadIndex = 0;
@@ -27,11 +33,6 @@ public class carMovement : MonoBehaviour
     public float distance = 0;
     public float speed = 15;
     // Start is called before the first frame update
-    public MyStuff stuf1 = new MyStuff(0, 0);
-    public MyStuff stuf2 = new MyStuff(0, 0);
-    public MyStuff stuf3 = new MyStuff(0, 0);
-    public Vector3 step = new Vector3();
-
     void Start()
     {
         
