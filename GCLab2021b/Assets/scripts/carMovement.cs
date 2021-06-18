e5cdf31579cdf02b6f51d2c37b8b642880394fdd
RowaidaMahameed <79975439+RowaidaMahameed@users.noreply.github.com>
2021-06-18 14:41:10 +0300

    addingWeightsAndPrices

--- GCLab2021b/Assets/scripts/carMovement.cs
+++ GCLab2021b/Assets/scripts/carMovement.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 using System;
 
 
-public class MyStuff : MonoBehaviour
+public class MyStuff
 {
     public int type;
     public int price;
@@ -20,22 +22,30 @@ public class MyStuff : MonoBehaviour
 
 public class carMovement : MonoBehaviour
 {
+    public GameObject[] weights = new GameObject[3] ;
+    public GameObject[] prices = new GameObject[3];
+
     const int stuffsNumber = 3;
     public GameObject[] allStuff = new GameObject[stuffsNumber];
-    public MyStuff stuff1 = new MyStuff(0,0,0);
-    public MyStuff stuff2 = new MyStuff(1,0, 0);
-    public MyStuff stuff3 = new MyStuff(2,0, 0);
+    public MyStuff stuff1 = new MyStuff(0,10,10);
+    public MyStuff stuff2 = new MyStuff(1,60, 1000);
+    public MyStuff stuff3 = new MyStuff(2,30, 200);
+    public MyStuff[] All = new MyStuff[3];
     const int roadsNumber = 10;
     public GameObject[] Roads = new GameObject[roadsNumber];
     public int roadIndex = 0;
     public GameObject car;
     public GameObject mycamera;
     public float distance = 0;
+    public float textdist = 0;
     public float speed = 15;
     // Start is called before the first frame update
+
     void Start()
     {
-        
+        All[0] = stuff1;
+        All[1] = stuff2;
+        All[2] = stuff3;
     }
 
     // Update is called once per frame
@@ -44,12 +54,19 @@ public class carMovement : MonoBehaviour
         car.gameObject.transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
         mycamera.gameObject.transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
         distance += speed * Time.deltaTime;
-        if(distance >= 30)
+        textdist += speed * Time.deltaTime;
+        if (distance >= 30)
         {
             distance = 0;
-            Roads[roadIndex].gameObject.transform.position += new Vector3(0, 0, 30* roadsNumber);
+            Roads[roadIndex].gameObject.transform.position += new Vector3(0, 0, 30 * roadsNumber);
             roadIndex = (roadIndex + 1) % roadsNumber;
 
         }
+        if (textdist >= 50)
+        {
+            weights[0].GetComponent<Text>().text = "100";
+
+        }
     }
+    
 }
